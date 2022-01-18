acb1fb0c6a408e0cdd938fa0e68a188b7c4c413d
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2022-01-18 21:19:52 +0200

    added firebase storage, database & geolocation func on submit

--- src/pages/CreateListing.js
+++ src/pages/CreateListing.js
@@ -3,9 +3,16 @@ import { useState, useEffect, useRef } from "react";
 //router
 import { useNavigate } from "react-router-dom";
 //firebase
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { db } from '../firebase.config';
+//uuid - unique id generator package
+import { v4 as uuidv4 } from 'uuid';
 //components
 import Spinner from '../components/Spinner';
+//toast
+import { toast } from 'react-toastify';
 
 
 const CreateListing = () => {
@@ -59,9 +66,114 @@ const CreateListing = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isMounted])
 
-    const onSubmit = (e) => {
+    //send data to firebase
+    const onSubmit = async (e) => {
         e.preventDefault()
-        console.log(formData);
+        setLoading(true)
+
+        //validate prices
+        if (discountedPrice >= regularPrice) {
+            setLoading(false)
+            toast.error('Discounted price needs to be less than regular price')
+            return
+        }
+        //set max 6 images
+        if (images.length > 6) {
+            setLoading(false)
+            toast.error('Max 6 images')
+            return
+        }
+
+        let geolocation = {}
+        let location
+
+        //if geolocation is enabled, use google api else use the form input values for lat and lng
+        if (geolocationEnabled) {
+            const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${ address }&key=${ process.env.REACT_APP_GEOCODE_API_KEY }`)
+            const data = await response.json()
+            geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
+            geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
+            location = data.status === 'ZERO_RESULTS' ? undefined : data.results[0]?.formatted_address
+            //check if location is undefined
+            if (location === undefined || location.includes('undefined')) {
+                setLoading(false)
+                toast.error('Please enter a valid address')
+                return
+            }
+        } else {
+            geolocation.lat = latitude
+            geolocation.lng = longitude
+            location = address
+        }
+
+        //store image in firebase & get the download URL ---> https://firebase.google.com/docs/storage/web/upload-files
+        const storeImage = async (image) => {
+            return new Promise((resolve, reject) => {
+                const storage = getStorage()
+                const fileName = `${ auth.currentUser.uid }-${ image.name }-${ uuidv4() }`
+                const storageRef = ref(storage, 'images/' + fileName)
+                const uploadTask = uploadBytesResumable(storageRef, image);
+
+                uploadTask.on('state_changed',
+                    (snapshot) => {
+                        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                        console.log('Upload is ' + progress + '% done');
+                        switch (snapshot.state) {
+                            case 'paused':
+                                console.log('Upload is paused');
+                                break;
+                            case 'running':
+                                console.log('Upload is running');
+                                break;
+                        }
+                    },
+                    (error) => {
+                        reject(error)
+                    },
+                    () => {
+                        // Handle successful uploads on complete
+                        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                            resolve(downloadURL);
+                        });
+                    }
+                );
+
+            })
+        }
+
+        //call storeImage for all images
+        const imgUrls = await Promise.all(
+            [...images].map((image) => storeImage(image))
+        ).catch(() => {
+            setLoading(false)
+            toast.error('Images not uploaded')
+            return
+        })
+
+        //create copy of formData and add extra values
+        const formDataCopy = {
+            ...formData,
+            imgUrls,
+            geolocation,
+            timestamp: serverTimestamp()
+        }
+
+        //delete images from formDataCopy since we added imgUrls above
+        delete formDataCopy.images
+        //delete address fromm formDataCopy 
+        delete formDataCopy.address
+        //add location to formDataCopy
+        location && (formDataCopy.location = location)
+        //if there is no offer (if offer is false), delete discountedPice from formDataCopy
+        !formDataCopy.offer && delete formDataCopy.discountedPrice
+
+        //add doc to firebase
+        const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
+        setLoading(false)
+        toast.success('Listing saved')
+        navigate(`/category/${ formDataCopy.type }/${ docRef.id }`)
     }
 
     //this handles all the input changes
@@ -81,7 +193,7 @@ const CreateListing = () => {
         if (e.target.files) {
             setFormData((prevState) => ({
                 ...prevState,
-                images: e.target.files
+                images: e.target.files,
             }))
         }
 
